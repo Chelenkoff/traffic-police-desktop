56080d84fe76fb318e53ea8691fee41b58ab7a72
Chelenkoff <resiloveca94@gmail.com>
2016-04-03 23:06:02 +0300

    AddDriverOwner improvements, CoutryListCyrillic.txt resource added

--- TrafficPoliceDesktopApp/View/AddDriverOwnerView.xaml.cs
+++ TrafficPoliceDesktopApp/View/AddDriverOwnerView.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -23,5 +25,37 @@ namespace TrafficPoliceDesktopApp.View
         {
             InitializeComponent();
         }
+
+  
+
+        
+
+        public List<string> getCountryList()
+        {
+            List<string> LogList = new List<string>();
+            try
+            {
+                var logFile = File.ReadAllLines(@"Resources\Textfiles\CountryListCyrillic.txt");
+                LogList = new List<string>(logFile);
+            }
+            catch
+            {
+                MessageBox.Show("hahah");
+            }
+            
+             
+            return LogList;
+
+        }
+
+
+
+        private void UserControl_Initialized(object sender, EventArgs e)
+        {
+            comboBoxCountries.ItemsSource = getCountryList();
+            comboBoxCountries.SelectedItem = "България";
+        }
+
+
     }
 }
