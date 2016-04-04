c7f8362a38ff8d08d47c8799fb13c853e8f079a8
Chelenkoff <resiloveca94@gmail.com>
2016-04-04 19:48:42 +0300

    AddDriver Categories module added

--- TrafficPoliceDesktopApp/View/AddDriverOwnerView.xaml.cs
+++ TrafficPoliceDesktopApp/View/AddDriverOwnerView.xaml.cs
@@ -40,7 +40,7 @@ namespace TrafficPoliceDesktopApp.View
             }
             catch
             {
-                MessageBox.Show("hahah");
+                MessageBox.Show("File does not exist!");
             }
             
              
@@ -54,6 +54,100 @@ namespace TrafficPoliceDesktopApp.View
         {
             comboBoxCountries.ItemsSource = getCountryList();
             comboBoxCountries.SelectedItem = "България";
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            clearForm();
+        }
+
+        private void clearForm()
+        {
+            txtBoxId.Text = "";
+            txtBoxFirstName.Text = "";
+            txtBoxSecondName.Text = "";
+            txtBoxLastName.Text = "";
+            comboBoxCountries.SelectedItem = "България";
+            txtBoxBirthPlace.Text = "";
+            txtBoxResidence.Text = "";
+            upDownRemainingPts.Value = 39;
+            txtBoxLicenceNum.Text = "";
+            txtBoxIssuedBy.Text = "";
+            datePickerIssuedDate.SelectedDate = null;
+            datePickerExpiryDate.SelectedDate = null;
+            clearCategories();
+
+        }
+
+        private void clearCategories()
+        {
+            datePickerA1AcquiryDate.SelectedDate = null;
+            datePickerA1ExpiryDate.SelectedDate = null;
+            txtBoxA1Restrictions.Text = "";
+
+            datePickerAAcquiryDate.SelectedDate = null;
+            datePickerAExpiryDate.SelectedDate = null;
+            txtBoxARestrictions.Text = "";
+
+            datePickerB1AcquiryDate.SelectedDate = null;
+            datePickerB1ExpiryDate.SelectedDate = null;
+            txtBoxB1Restrictions.Text = "";
+
+            datePickerBAcquiryDate.SelectedDate = null;
+            datePickerBExpiryDate.SelectedDate = null;
+            txtBoxBRestrictions.Text = "";
+
+            datePickerC1AcquiryDate.SelectedDate = null;
+            datePickerC1ExpiryDate.SelectedDate = null;
+            txtBoxC1Restrictions.Text = "";
+
+            datePickerCAcquiryDate.SelectedDate = null;
+            datePickerCExpiryDate.SelectedDate = null;
+            txtBoxCRestrictions.Text = "";
+
+            datePickerD1AcquiryDate.SelectedDate = null;
+            datePickerD1ExpiryDate.SelectedDate = null;
+            txtBoxD1Restrictions.Text = "";
+
+            datePickerDAcquiryDate.SelectedDate = null;
+            datePickerDExpiryDate.SelectedDate = null;
+            txtBoxDRestrictions.Text = "";
+
+            datePickerBEAcquiryDate.SelectedDate = null;
+            datePickerBEExpiryDate.SelectedDate = null;
+            txtBoxBERestrictions.Text = "";
+
+            datePickerC1EAcquiryDate.SelectedDate = null;
+            datePickerC1EExpiryDate.SelectedDate = null;
+            txtBoxC1ERestrictions.Text = "";
+
+            datePickerCEAcquiryDate.SelectedDate = null;
+            datePickerCEExpiryDate.SelectedDate = null;
+            txtBoxCERestrictions.Text = "";
+
+            datePickerD1EAcquiryDate.SelectedDate = null;
+            datePickerD1EExpiryDate.SelectedDate = null;
+            txtBoxD1ERestrictions.Text = "";
+
+            datePickerDEAcquiryDate.SelectedDate = null;
+            datePickerDEExpiryDate.SelectedDate = null;
+            txtBoxDERestrictions.Text = "";
+
+            datePickerTtbAcquiryDate.SelectedDate = null;
+            datePickerTtbExpiryDate.SelectedDate = null;
+            txtBoxTtbRestrictions.Text = "";
+
+            datePickerTtmAcquiryDate.SelectedDate = null;
+            datePickerTtmExpiryDate.SelectedDate = null;
+            txtBoxTtmRestrictions.Text = "";
+
+            datePickerTktAcquiryDate.SelectedDate = null;
+            datePickerTktExpiryDate.SelectedDate = null;
+            txtBoxTktRestrictions.Text = "";
+
+            datePickerMAcquiryDate.SelectedDate = null;
+            datePickerMExpiryDate.SelectedDate = null;
+            txtBoxMRestrictions.Text = "";
         }
 
 
